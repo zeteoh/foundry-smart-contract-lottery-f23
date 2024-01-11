ab72078b1c5f456c130a543480f2e5caf2d05583
zeteoh <teohzeloong12@gmail.com>
2024-01-11 19:17:04 +0800

    error: added custom error

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -29,13 +29,19 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRF2
  */
 contract Raffle {
+    error Raffle__NotEnoughtEthSent();
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        //require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughtEthSent();
+        }
+    }
 
     function pickWinner() public {}
 
